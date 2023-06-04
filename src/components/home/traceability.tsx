f8a1954a30af6b1c8c3d069a7753a23a1fd65822
614Poste <a1120150019@utch.edu.mx>
2023-06-03 20:35:53 -0600

    UPDATE

--- src/components/home/traceability.tsx
+++ src/components/home/traceability.tsx
@@ -79,10 +79,10 @@ export default function Example() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-3xl sm:text-center">
           <p className="mb-6 mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Todo lo que necesitas en TerraPlot
+            Todo lo que necesitas 
           </p>
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
-             Todo a tu alcance
+             A tu alcance
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl"></p>
           <p className="mt-6 text-lg leading-8 text-gray-300">
