8015fda3e06efc7733cc9d44eb6ef7351832df5d
Nathan Lazo <nathanlazo@outlook.com>
2023-06-06 17:40:53 -0600

    hot fix

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -66,7 +66,7 @@ const Auth: FC = () => {
         error: "Sign in failed!",
       })
       .then(async () => {
-        await router.push("/marketplace");
+        await router.push("/dashboard");
       })
       .catch((error) => {
         console.log(error);
